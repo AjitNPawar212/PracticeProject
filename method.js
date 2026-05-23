3e50dd1cb2ab294f5c791004486f410673463f14
AjitNPawar212 <ajitspawar212@gmail.com>
2026-05-23 20:45:43 +0530

    First commit

---
+++ method.js
@@ -0,0 +1,16 @@
+//We would Learn How to use the Function in JS::
+
+
+
+function sum(a, callback){
+    
+
+    
+    setTimeout(()=>{
+        let b = 20;
+        console.log(b);
+    })}
+
+    sum(25, ()=>{
+        console.log("Sum of the Values is", 100);
+    });
